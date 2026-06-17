5cef2fae31205852f5a4d00d9eb69742ee9ec660
sigittriyono <168503107+sigittriyono@users.noreply.github.com>
2026-06-17 13:21:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -266,7 +266,7 @@ if "analysis_done" not in st.session_state:
 # ─────────────────────────────────────────────────────────────────────────────
 # MODEL LOADING (cached)
 # ─────────────────────────────────────────────────────────────────────────────
-DRIVE_URL = "https://drive.google.com/file/d/1m8elxjlJej3t2qjTk_SgWOctMR574k9F/view?usp=sharing"
+DRIVE_URL = "https://drive.google.com/file/d/17qQx7SaA7P9fZitZQtqtMbROPwuFK_ee/view?usp=sharing"
 download_model_if_missing(DRIVE_URL)
 
 @st.cache_resource(show_spinner=False)
